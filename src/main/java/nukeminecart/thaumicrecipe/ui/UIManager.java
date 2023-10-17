8685be3f2595d461282fb98e8a32894dda7066a0
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-17 17:53:29 -0500

    Move all stage related stuff to UIManager and change HomeUI to just be scene and event handler related

--- src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
@@ -1,11 +1,36 @@
 package main.java.nukeminecart.thaumicrecipe.ui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import main.java.nukeminecart.thaumicrecipe.ui.home.HomeUI;
 
-public class UIManager {
+public class UIManager extends Application{
+    public static Stage stage;
     public static void main(String[] args){
-        Application.launch(HomeUI.class);
-
+        launch();
     }
+
+    public static void loadScreen(Scene scene) {
+        Platform.runLater(() -> {
+            stage.setScene(scene);
+            stage.show();
+        });
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        UIManager.stage = stage;
+
+        Scene scene = new Scene(HomeUI.getScene(), 750, 500);
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Thaumic Recipe Tweaker Home");
+        UIManager.loadScreen(scene);
+    }
+
+
+
 }
