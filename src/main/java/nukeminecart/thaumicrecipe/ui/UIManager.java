803e276abaf5054e30add6be6320f1292221db2c
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-19 21:47:58 -0500

    Work on adding the load recipe from folder functionality

--- src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
@@ -11,7 +11,14 @@ import main.java.nukeminecart.thaumicrecipe.ui.home.HomeUI;
 import java.util.Objects;
 
 public class UIManager extends Application{
+
+    public static String separator;
+    public static String minecraftDir;
+    public static String loadedRecipe;
     public static void main(String[] args){
+        separator = System.getProperty("file.separator");
+        minecraftDir = args.length > 0 ? args[0] : null;
+        loadedRecipe = args.length > 1 ? args[1] : null;
         launch();
     }
     public static Stage stage;
--- src/main/java/nukeminecart/thaumicrecipe/ui/home/HomeUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/home/HomeUI.java
@@ -4,6 +4,8 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
@@ -11,12 +13,20 @@ import main.java.nukeminecart.thaumicrecipe.ui.UIManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+
+import static main.java.nukeminecart.thaumicrecipe.ui.UIManager.minecraftDir;
+import static main.java.nukeminecart.thaumicrecipe.ui.UIManager.separator;
 
 public class HomeUI{
     public SplitMenuButton newRecipeButton;
     public SplitMenuButton loadRecipeButton;
+    public MenuButton fileField;
     private double x,y;
+    private List<String> fileList;
+
 
     private String loadOption = "fromConfig";
     private String newOption = "recipeGroup";
@@ -41,19 +51,36 @@ public class HomeUI{
         if(loadOption.equals("fromOther")) {
             FileChooser chooser = new FileChooser();
             chooser.setTitle("Open File");
+
             FileChooser.ExtensionFilter extFilter =
                     new FileChooser.ExtensionFilter("RECIPE files (*.rcp)", "*.rcp");
             chooser.getExtensionFilters().add(extFilter);
             chooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-            chooser.showOpenDialog(UIManager.stage.getOwner());
+            File fileChosen = chooser.showOpenDialog(UIManager.stage.getOwner());
+
+
         } else if (loadOption.equals("fromFolder")) {
 
-        }else{
 
+        }else{
+            
         }
+    }
+    private void loadRecipeDirectory(){
+        if(minecraftDir != null){
+            File directory = new File(minecraftDir+separator+"thaumicrecipe");
+            if(!directory.exists()){
+                directory.mkdirs();
+            }
+
+            fileList.addAll(Arrays.asList(Objects.requireNonNull(directory.list())));
+        }
+
+
     }
 
     @FXML private void fromConfig() {
+        fileField.setVisible(false);
         loadOption = "fromConfig";
         loadRecipeButton.setText("Load From Config");
         loadRecipeButton.setPrefWidth(180);
@@ -61,12 +88,23 @@ public class HomeUI{
     }
 
     @FXML private void fromFolder() {
+        fileField.setVisible(true);
+        loadRecipeDirectory();
+        fileField.getItems().clear();
+        if(fileList != null) {
+            for (String name : fileList) {
+                fileField.getItems().add(new MenuItem(name));
+            }
+        }else{
+            fileField.getItems().add(new MenuItem("No .rcp files found"));
+        }
         loadOption = "fromFolder";
         loadRecipeButton.setText("Load From Folder");
         loadRecipeButton.setPrefWidth(180);
     }
 
     @FXML private void fromOther() {
+        fileField.setVisible(false);
         loadOption = "fromOther";
         loadRecipeButton.setText("Load From Other");
         loadRecipeButton.setPrefWidth(170);
