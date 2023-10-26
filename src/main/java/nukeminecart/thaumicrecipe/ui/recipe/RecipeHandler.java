e951b5fa4286be1d885dc25b76af0f201e89607c
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-25 21:33:18 -0500

    Check for error conditions

--- src/main/java/nukeminecart/thaumicrecipe/ui/recipe/RecipeHandler.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/RecipeHandler.java
@@ -1,52 +1,62 @@
 package main.java.nukeminecart.thaumicrecipe.ui.recipe;
 
-import main.java.nukeminecart.thaumicrecipe.ui.home.HomeUI;
+import javafx.scene.control.Label;
+import main.java.nukeminecart.thaumicrecipe.ui.ErrorWarning;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.IFileParser;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.manager.RecipeManagerUI;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static main.java.nukeminecart.thaumicrecipe.ui.UIManager.loadedRecipe;
 
 public class RecipeHandler {
     public static File file;
+    private static Label newWarning;
     public static void loadConfigRecipe() throws IOException {
-        file = IFileParser.getFolderFile(loadedRecipe);
-        String[] contents = IFileParser.readFile(file);
-        RecipeManagerUI.loadManager(contents);
+        if(loadedRecipe!=null) {
+            file = IFileParser.getFolderFile(loadedRecipe.endsWith(".rcp") ? loadedRecipe : loadedRecipe + ".rcp");
+            List<String> contents = IFileParser.readFile(file);
+            RecipeManagerUI.loadManager(file.getName(),contents);
+        }else{
+            ErrorWarning.invalidFileError(newWarning,"No file in config");
+        }
     }
     public static void loadFolderRecipe(String filename) throws IOException {
         file = IFileParser.getFolderFile(filename);
-        String[] contents = IFileParser.readFile(file);
-        RecipeManagerUI.loadManager(contents);
+        List<String> contents = IFileParser.readFile(file);
+        RecipeManagerUI.loadManager(filename,contents);
 
     }
     public static void loadOtherRecipe(String path) throws IOException {
         file = new File(path);
-        String[] contents = IFileParser.readFile(IFileParser.checkExists(file) ? file : null) ;
-        RecipeManagerUI.loadManager(contents);
+        List<String> contents = IFileParser.readFile(IFileParser.checkExists(file) ? file : null) ;
+        RecipeManagerUI.loadManager(file.getName(),contents);
     }
     public static void newRecipeGroup(String name) throws IOException {
         file = IFileParser.getFolderFile(name.endsWith(".rcp") ? name : name+".rcp");
         if(file.exists()){
-            new HomeUI().throwNewWarning("File already exists");
+            ErrorWarning.invalidFileError(newWarning,"File already exists");
         }else{
-            RecipeManagerUI.loadManager(null);
+            RecipeManagerUI.loadManager(name,null);
         }
     }
     public static void newRecipe(String name) throws IOException {
         file = IFileParser.getFolderFile(name.endsWith(".rcp") ? name : name+".rcp");
         if(file.exists()){
-            //TODO Fix crash -> new instance of class (Don't do a new instance)
-            new HomeUI().throwNewWarning("File already exists");
+            ErrorWarning.invalidFileError(newWarning,"File already exists");
         }else{
-            RecipeManagerUI.loadManager(null);
+            RecipeManagerUI.loadManager(name,null);
         }
+    }
+    public static void setLabel(Label newWarning) {
+        RecipeHandler.newWarning=newWarning;
     }
 
     public static void saveToFile(File savefile, String[] contents){
         //TODO
     }
+
 }
 
