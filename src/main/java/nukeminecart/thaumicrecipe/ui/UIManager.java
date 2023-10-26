7f775d31e467e3b2b76bd5a0e33b95329f83cc18
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-25 21:34:28 -0500

    Change the loadscreen to be of a parent, since size of screen doesn't change

--- src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/UIManager.java
@@ -2,6 +2,7 @@ package main.java.nukeminecart.thaumicrecipe.ui;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -13,15 +14,18 @@ import java.util.Objects;
 public class UIManager extends Application{
 
     public static String separator, recipeDir, loadedRecipe;
+
     public static void main(String[] args){
         separator = System.getProperty("file.separator");
         recipeDir = args.length > 0 ? args[0]+separator+"thaumicrecipe"+separator : null;
         loadedRecipe = args.length > 1 ? args[1] : null;
         launch();
+
     }
     public static Stage stage;
 
-    public static void loadScreen(Scene scene) {
+    public static void loadScreen(Parent parent) {
+        Scene scene = new Scene(parent, 750, 500);
         Platform.runLater(() -> {
             stage.setScene(scene);
             stage.show();
@@ -31,14 +35,10 @@ public class UIManager extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         UIManager.stage = stage;
-
-        Scene scene = new Scene(HomeUI.getScene(), 750, 500);
-
         stage.initStyle(StageStyle.UNDECORATED);
         stage.getIcons().add(new Image(Objects.requireNonNull(UIManager.class.getResourceAsStream("resources/icon.png"))));
         stage.setTitle("Thaumic Recipe Tweaker Home");
-
-        UIManager.loadScreen(scene);
+        UIManager.loadScreen(HomeUI.getScene());
     }
 
 
