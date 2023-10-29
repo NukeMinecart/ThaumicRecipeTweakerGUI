d919307948d9b8f344946eddb393e682eef7b3e0
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-28 20:42:33 -0500

    Begin adding the recipe shape editor functionality

--- src/main/java/nukeminecart/thaumicrecipe/ui/recipe/editor/shape/RecipeShapeUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/editor/shape/RecipeShapeUI.java
@@ -4,14 +4,19 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.input.*;
 import main.java.nukeminecart.thaumicrecipe.ui.UIManager;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.Recipe;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 public class RecipeShapeUI {
+
+    private static final DataFormat SERIALIZED_MIME_TYPE = new DataFormat("application/x-java-serialized-object");
+
     public static Recipe recipe;
     private double x,y;
 
@@ -38,5 +43,43 @@ public class RecipeShapeUI {
     @FXML private void paneDragged(MouseEvent me){
         UIManager.stage.setX(x + me.getScreenX());
         UIManager.stage.setY(y + me.getScreenY());
+    }
+
+
+    private void enableDragAndDrop(List<ListView<String>> listViews, ListView<String> sourceListView) {
+        sourceListView.setOnDragDetected(event -> {
+            Dragboard dragboard = sourceListView.startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            int selectedIdx = sourceListView.getSelectionModel().getSelectedIndex();
+            content.put(SERIALIZED_MIME_TYPE, selectedIdx);
+            dragboard.setContent(content);
+            event.consume();
+        });
+
+        for (ListView<String> targetListView : listViews) {
+            targetListView.setOnDragOver(event -> {
+                if (event.getGestureSource() != targetListView && event.getDragboard().hasContent(SERIALIZED_MIME_TYPE)) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+                event.consume();
+            });
+
+            targetListView.setOnDragDropped(event -> {
+                Dragboard dragboard = event.getDragboard();
+                boolean success = false;
+
+                if (dragboard.hasContent(SERIALIZED_MIME_TYPE)) {
+                    int draggedIdx = (Integer) dragboard.getContent(SERIALIZED_MIME_TYPE);
+                    String item = sourceListView.getItems().get(draggedIdx);
+                    targetListView.getItems().add(item);
+                    sourceListView.getItems().remove(draggedIdx);
+
+                    success = true;
+                }
+
+                event.setDropCompleted(success);
+                event.consume();
+            });
+        }
     }
 }
