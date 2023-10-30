e9258035c49362285c80b80b05841363957d19cc
NukeMinecart <bradley.chumanov@gmail.com>
2023-10-29 20:26:03 -0500

    Add the dragging, copying and dropping functionality

--- src/main/java/nukeminecart/thaumicrecipe/ui/recipe/editor/shape/RecipeShapeUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/editor/shape/RecipeShapeUI.java
@@ -1,6 +1,8 @@
 package main.java.nukeminecart.thaumicrecipe.ui.recipe.editor.shape;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,26 +12,49 @@ import main.java.nukeminecart.thaumicrecipe.ui.UIManager;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.Recipe;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class RecipeShapeUI {
 
-    private static final DataFormat SERIALIZED_MIME_TYPE = new DataFormat("application/x-java-serialized-object");
+    public static ObservableList<String> ingredientList = FXCollections.observableArrayList();
 
-    public static Recipe recipe;
+    @FXML
+    private ListView<String> ingredients, craft1, craft2, craft3, craft4, craft5, craft6, craft7, craft8, craft9;
+    List<ListView<String>> targetListViews = new ArrayList<>();
+
     private double x,y;
 
     public static Parent getScene() throws IOException {
         return FXMLLoader.load(Objects.requireNonNull(RecipeShapeUI.class.getResource("RecipeShapeUI.fxml")));
     }
     public static void launchEditor(Recipe recipe) throws IOException {
-        RecipeShapeUI.recipe = recipe;
+        RecipeShapeUI.ingredientList.addAll(recipe.getIngredients());
         UIManager.loadScreen(getScene());
     }
 
     @FXML
     public void initialize(){
+        ingredients.setItems(ingredientList);
+        targetListViews.add(craft1);
+        targetListViews.add(craft2);
+        targetListViews.add(craft3);
+        targetListViews.add(craft4);
+        targetListViews.add(craft5);
+        targetListViews.add(craft6);
+        targetListViews.add(craft7);
+        targetListViews.add(craft8);
+        targetListViews.add(craft9);
+        ingredients.setOnDragDetected(event -> dragItem(ingredients));
+
+        for (ListView<String> listView : targetListViews){
+            listView.setOnDragDetected(event -> dragItem(listView));
+            listView.setOnDragOver(event -> allowDrop(listView, event));
+            listView.setOnDragDropped(event -> copyItem(listView, event));
+        }
+
+
     }
 
     @FXML private void closeScreen() {
@@ -46,40 +71,43 @@ public class RecipeShapeUI {
     }
 
 
-    private void enableDragAndDrop(List<ListView<String>> listViews, ListView<String> sourceListView) {
-        sourceListView.setOnDragDetected(event -> {
-            Dragboard dragboard = sourceListView.startDragAndDrop(TransferMode.MOVE);
-            ClipboardContent content = new ClipboardContent();
-            int selectedIdx = sourceListView.getSelectionModel().getSelectedIndex();
-            content.put(SERIALIZED_MIME_TYPE, selectedIdx);
-            dragboard.setContent(content);
-            event.consume();
-        });
+    private void dragItem(ListView<String> listView) {
+        Dragboard db = listView.startDragAndDrop(TransferMode.COPY);
+        ClipboardContent content = new ClipboardContent();
+        content.putString(listView.getSelectionModel().getSelectedItem());
+        if(listView!=ingredients){
+            listView.getItems().clear();
+        }
+        db.setContent(content);
+    }
 
-        for (ListView<String> targetListView : listViews) {
-            targetListView.setOnDragOver(event -> {
-                if (event.getGestureSource() != targetListView && event.getDragboard().hasContent(SERIALIZED_MIME_TYPE)) {
-                    event.acceptTransferModes(TransferMode.MOVE);
-                }
-                event.consume();
-            });
+    private void allowDrop(ListView<String> listView, DragEvent event) {
+        if (event.getDragboard().hasString()) {
+            if (listView.getItems().isEmpty()) {
+                // If the target list is empty, allow copy
+                event.acceptTransferModes(TransferMode.COPY);
+            } else {
+                // If the target list already has an item, allow copy and clear
+                event.acceptTransferModes(TransferMode.COPY, TransferMode.MOVE);
+            }
+        }
+    }
 
-            targetListView.setOnDragDropped(event -> {
-                Dragboard dragboard = event.getDragboard();
-                boolean success = false;
+    private void copyItem(ListView<String> listView, DragEvent event) {
+        Dragboard db = event.getDragboard();
 
-                if (dragboard.hasContent(SERIALIZED_MIME_TYPE)) {
-                    int draggedIdx = (Integer) dragboard.getContent(SERIALIZED_MIME_TYPE);
-                    String item = sourceListView.getItems().get(draggedIdx);
-                    targetListView.getItems().add(item);
-                    sourceListView.getItems().remove(draggedIdx);
+        if (db.hasString()) {
+            String draggedItem = db.getString();
+            ObservableList<String> targetList = listView.getItems();
 
-                    success = true;
-                }
-
-                event.setDropCompleted(success);
-                event.consume();
-            });
+            if (targetList.isEmpty()) {
+                // If the target list is empty, just add the dragged item
+                targetList.add(draggedItem);
+            } else {
+                // If the target list already has an item, clear it and add the dragged item
+                targetList.clear();
+                targetList.add(draggedItem);
+            }
         }
     }
 }
