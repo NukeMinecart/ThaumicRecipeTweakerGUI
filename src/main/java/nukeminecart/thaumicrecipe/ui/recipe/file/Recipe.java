fe6e9ef198621b85289730450237d0826a9f32f2
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-01 20:50:17 -0500

    Add javadoc and consistent formatting

--- src/main/java/nukeminecart/thaumicrecipe/ui/recipe/file/Recipe.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/file/Recipe.java
@@ -1,12 +1,30 @@
 package main.java.nukeminecart.thaumicrecipe.ui.recipe.file;
 
+import java.util.List;
+
+/**
+ * A type used in ThaumicRecipeTweakerGUI that contains information about a specific recipe
+ */
 public class Recipe {
 
     private final String name, input, output, type;
     private final String[] ingredients, aspects;
     private final int vis;
-    private final Object[] shape;
-    public Recipe(String name, String type, String input, String[] ingredients, String output, int vis, String[] aspects, String... shape){
+    private final String[] shape;
+
+    /**
+     * The constructor to create a new {@link Recipe}
+     *
+     * @param name        the name
+     * @param type        the type
+     * @param input       the input item
+     * @param ingredients a list of ingredients
+     * @param output      the output item
+     * @param vis         the vis cost
+     * @param aspects     a list of aspects
+     * @param shape       a string object containing the shape
+     */
+    public Recipe(String name, String type, String input, String[] ingredients, String output, int vis, String[] aspects, String... shape) {
         this.name = name;
         this.type = type;
         this.input = input;
@@ -16,24 +34,79 @@ public class Recipe {
         this.aspects = aspects;
         this.shape = shape;
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the recipe
+     *
+     * @return the name as a {@link String}
+     */
+    public String getName() {
         return this.name;
     }
-    public String getInput(){return this.input;}
 
-    public String[] getAspects(){
-        return this.aspects;
+    /**
+     * Gets the type of recipe
+     *
+     * @return the type as a {@link String}
+     */
+    public String getType() {
+        return this.type;
     }
-    public String[] getIngredients(){
+
+    /**
+     * Gets the input item of a recipe
+     *
+     * @return the input item as a {@link String}
+     */
+    public String getInput() {
+        return this.input;
+    }
+
+    /**
+     * Gets the ingredients of a recipe
+     *
+     * @return the ingredients as a {@link List<String>}
+     */
+    public String[] getIngredients() {
         return this.ingredients;
     }
-    public String getOutput(){return this.output;}
 
-    public Object[] getShape(){
-        return this.shape;
+    /**
+     * Gets the output item of a recipe
+     *
+     * @return the output item as a {@link String}
+     */
+    public String getOutput() {
+        return this.output;
     }
-    public int getVis(){
+
+    /**
+     * Gets the vis cost of a recipe
+     *
+     * @return the vis cost as a {@link Integer}
+     */
+    public int getVis() {
         return this.vis;
     }
-    public String getType(){return this.type;}
+
+    /**
+     * Gets the aspects of a recipe
+     *
+     * @return the aspects as a {@link List<String>}
+     */
+    public String[] getAspects() {
+        return this.aspects;
+    }
+
+
+    /**
+     * Gets the shape of a recipe
+     *
+     * @return the shape as a {@link String} array
+     */
+    public String[] getShape() {
+        return this.shape;
+    }
+
+
 }
