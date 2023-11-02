23c6825d16d906bfa3bc20c217ce6bff71f69fa6
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-01 20:32:55 -0500

    Add javadoc and consistent formatting

--- src/main/java/nukeminecart/thaumicrecipe/ui/recipe/manager/RecipeManagerUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/manager/RecipeManagerUI.java
@@ -1,6 +1,5 @@
 package main.java.nukeminecart.thaumicrecipe.ui.recipe.manager;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -9,87 +8,99 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
+import main.java.nukeminecart.thaumicrecipe.ui.ThaumicRecipeUI;
 import main.java.nukeminecart.thaumicrecipe.ui.UIManager;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.editor.RecipeEditorUI;
+import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.FileParser;
 import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.Recipe;
-import main.java.nukeminecart.thaumicrecipe.ui.recipe.file.versions.FileParserV1;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
-public class RecipeManagerUI {
+/**
+ * The class that contains all the controller elements and logic for the RecipeManagerUI parent
+ */
+
+public class RecipeManagerUI extends ThaumicRecipeUI {
 
 
-
-    public static ObservableList<Recipe> recipes = FXCollections.observableArrayList();
-    public static HashMap<String, Recipe> recipeEditorMap = new HashMap<>();
-
+    public static final ObservableList<Recipe> recipes = FXCollections.observableArrayList();
+    public static final HashMap<String, Recipe> recipeEditorMap = new HashMap<>();
+    private static String stringTitle;
     @FXML
     public ListView<Recipe> recipeList;
     @FXML
     private Label title;
-    private static String stringTitle;
-    private double x,y;
 
+    /**
+     * Gets the parent container containing all the RecipeManagerUI elements
+     *
+     * @return the {@link Parent} container
+     * @throws IOException if RecipeManagerUI.fxml if not found
+     */
     public static Parent getScene() throws IOException {
 
         return FXMLLoader.load(Objects.requireNonNull(RecipeManagerUI.class.getResource("RecipeManagerUI.fxml")));
     }
 
+    /**
+     * Loads the RecipeManager scene and adds all the recipes to the listView
+     *
+     * @param name     the name of the file
+     * @param contents the contents of the file
+     * @throws IOException if getScene returns an invalid {@link Parent}
+     */
     public static void loadManager(String name, List<String> contents) throws IOException {
-        if(contents != null && contents.size()>1){
-            String version = contents.get(0);
-            version = version.replace("V","");
-            version = version.replace(";","");
-            contents.remove(0);
-            if(version.equals("1")) {
-                recipes.addAll(new FileParserV1().getRecipesFromString(contents));
-                refreshHashMap();
-            }
+        if (contents != null && contents.size() > 1) {
+            recipes.addAll(FileParser.getRecipesFromString(contents));
+            refreshHashMap();
         }
-        RecipeManagerUI.stringTitle = name.endsWith(".rcp") ? name : name+".rcp";
+        RecipeManagerUI.stringTitle = name.endsWith(".rcp") ? name : name + ".rcp";
         UIManager.loadScreen(getScene());
     }
 
-
-    @FXML
-    public void initialize(){
-        recipeList.setCellFactory(new RecipeCellFactory());
-        recipeList.setItems(recipes);
-        title.setText("Recipe Manager: "+stringTitle);
-        recipes.addListener((ListChangeListener<Recipe>) c -> refreshHashMap());
-    }
-
-    @FXML private void closeScreen() {
-        Platform.exit();
-    }
-    @FXML private void panePressed(MouseEvent me){
-        x = UIManager.stage.getX() - me.getScreenX();
-        y = UIManager.stage.getY() - me.getScreenY();
-    }
-
-    @FXML private void paneDragged(MouseEvent me){
-        UIManager.stage.setX(x + me.getScreenX());
-        UIManager.stage.setY(y + me.getScreenY());
-    }
-
-    public static void openEditor(String recipeName){
+    /**
+     * Loads the {@link RecipeEditorUI} and passes it the recipe to be displayed
+     *
+     * @param recipeName the recipe name to load the recipe editor with
+     */
+    public static void openEditor(String recipeName) {
         try {
             RecipeEditorUI.launchEditor(recipeEditorMap.get(recipeName));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    private static void refreshHashMap(){
-        for(Recipe recipe: recipes){
-            recipeEditorMap.put(recipe.getName(),recipe);
+
+    /**
+     * Updates the recipeEditorMap {@link HashMap} with the key as the name of the recipe and the values as the {@link Recipe} from the recipes {@link ObservableList}
+     */
+    private static void refreshHashMap() {
+        for (Recipe recipe : recipes) {
+            recipeEditorMap.put(recipe.getName(), recipe);
         }
     }
-    public static void refreshRecipes(){
+
+    /**
+     * Updates the recipes {@link ObservableList} with the values from the recipeEditorMap {@link HashMap}
+     */
+    public static void refreshRecipes() {
         recipes.clear();
         recipes.addAll(recipeEditorMap.values());
     }
+
+    /**
+     * FXML initialize event
+     */
+    @FXML
+    public void initialize() {
+        recipeList.setCellFactory(new RecipeCellFactory());
+        recipeList.setItems(recipes);
+        title.setText("Recipe Manager: " + stringTitle);
+        recipes.addListener((ListChangeListener<Recipe>) c -> refreshHashMap());
+    }
+
+
 }
