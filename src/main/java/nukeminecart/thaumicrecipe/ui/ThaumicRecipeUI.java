b0d92a362dd1a696168da2723d174f8bed3f465b
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-01 19:01:09 -0500

    Fix styling and add javadoc

--- src/main/java/nukeminecart/thaumicrecipe/ui/ThaumicRecipeUI.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/ThaumicRecipeUI.java
@@ -5,18 +5,34 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
 public class ThaumicRecipeUI {
-    //TODO finish this class and remove all instances of these methods
-    private double x,y;
+    private double x, y;
+
+    /**
+     * FXML event to close the screen
+     */
     @FXML
     private void closeScreen() {
         Platform.exit();
     }
-    @FXML private void panePressed(MouseEvent me){
+
+    /**
+     * FXML event that sets x and y to the current x and y position of the stage when pressed
+     *
+     * @param me the JavaFx mouse event
+     */
+    @FXML
+    private void panePressed(MouseEvent me) {
         x = UIManager.stage.getX() - me.getScreenX();
         y = UIManager.stage.getY() - me.getScreenY();
     }
 
-    @FXML private void paneDragged(MouseEvent me){
+    /**
+     * FXML event to change the x and y position of the stage when dragged
+     *
+     * @param me the JavaFx mouse event
+     */
+    @FXML
+    private void paneDragged(MouseEvent me) {
         UIManager.stage.setX(x + me.getScreenX());
         UIManager.stage.setY(y + me.getScreenY());
     }
