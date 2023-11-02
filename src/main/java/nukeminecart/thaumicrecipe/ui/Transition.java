b0fbd13fd7dae56ac94ec4ea69fe67ee79bd945f
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-01 19:06:32 -0500

    Fix styling and add javadoc

--- src/main/java/nukeminecart/thaumicrecipe/ui/Transition.java
+++ src/main/java/nukeminecart/thaumicrecipe/ui/Transition.java
@@ -5,12 +5,21 @@ import javafx.animation.FadeTransition;
 import javafx.scene.Node;
 import javafx.util.Duration;
 
+/**
+ * A class that contains all the transitions used in ThaumicRecipeTweakerGUI
+ */
 public class Transition {
-    public static void Fade(Duration durationMillis, int startOpacity, int endOpacity, Node node){
-        FadeTransition ft = new FadeTransition(Duration.millis(durationMillis.toMillis()), node);
+    /**
+     * Fade the given node linearly
+     * @param duration duration for animation
+     * @param startOpacity the opacity to start at
+     * @param endOpacity the opactiy to end at
+     * @param node the node to fade
+     */
+    public static void Fade(Duration duration, int startOpacity, int endOpacity, Node node) {
+        FadeTransition ft = new FadeTransition(Duration.millis(duration.toMillis()), node);
         ft.setFromValue(startOpacity);
         ft.setToValue(endOpacity);
-        ft.setAutoReverse(true);
         ft.play();
     }
 }
