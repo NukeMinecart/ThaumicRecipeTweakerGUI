010d2ba87ef5b7a99c4b492881cf4c91830364d5
NukeMinecart <bradley.chumanov@gmail.com>
2023-11-18 22:09:18 -0600

    Add a cell factory for the recipe search

---
+++ src/main/java/nukeminecart/thaumicrecipe/ui/recipe/editor/search/cell/SearchRecipeCellFactory.java
@@ -0,0 +1,72 @@
+package main.java.nukeminecart.thaumicrecipe.ui.recipe.editor.search.cell;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import main.java.nukeminecart.thaumicrecipe.ui.recipe.manager.RecipeManagerUI;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static main.java.nukeminecart.thaumicrecipe.ui.ThaumicRecipeConstants.stringArraySeparator;
+
+/**
+ * Class that contains the cell factory for {@link RecipeManagerUI}
+ */
+public class SearchRecipeCellFactory implements Callback<ListView<String>, ListCell<String>> {
+
+    private static String itemName;
+    private static String itemModid;
+    @FXML
+    private Label searchItem, searchItemModid;
+
+    private static Parent getScene() throws IOException {
+        return FXMLLoader.load(Objects.requireNonNull(SearchRecipeCellFactory.class.getResource("SearchRecipeCell.fxml")));
+    }
+
+    @Override
+    public ListCell<String> call(ListView<String> param) {
+        return new SearchRecipeCell();
+    }
+
+    /**
+     * FXML initialize method
+     */
+    @FXML
+    private void initialize() {
+        searchItem.setText(itemName);
+        searchItemModid.setText(itemModid);
+    }
+
+    /**
+     * Recipe Cell formatting and layout
+     */
+    public static class SearchRecipeCell extends ListCell<String> {
+        /*
+         Update an item in the list
+         */
+        @Override
+        protected void updateItem(String string, boolean empty) {
+
+            super.updateItem(string, empty);
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                try {
+                    SearchRecipeCellFactory.itemName = string.split(stringArraySeparator)[0];
+                    SearchRecipeCellFactory.itemModid = string.split(stringArraySeparator)[1];
+                    setGraphic(SearchRecipeCellFactory.getScene());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+
+    }
+}
